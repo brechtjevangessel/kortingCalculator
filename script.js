a9766982dc5627ce9169e8706175d7929abac061
Brechtje <brechtje.git@bram.blmgroep.nl>
2024-11-05 16:23:10 +0100

    changed function order for readability

--- script.js
+++ script.js
@@ -1,3 +1,142 @@
+// MAIN FUNCTION
+// Starts calculation progress, runs when calculation button is pressed
+
+function calculateDiscount(button) {
+    // makes sure that the function uses the input from the calculator the button is in
+    // this is only relevant if there is more than one calculator on screen
+    const container = button.closest('.calculatorContainer');
+
+    // GET ALL NESCESSARY DATA FROM USER INPUT
+
+    // data from inputs that stay the same
+    const price = container.querySelector('.priceInput').value * 100;
+    const quantity = Number(container.querySelector('.quantityInput').value);
+    const discountType = container.querySelector('.discountType').value;
+    
+    // data from inputs that may differ depending on which menu-options are selected
+    let discountPercentage = 0;
+    let shippingCost = 0;
+    
+    // changes shipping cost only if an input is detected, otherwise it stays 0
+    if (container.querySelector('.shippingCostInput') !== null) {
+        shippingCost = container.querySelector('.shippingCostInput').value * 100;
+    }
+
+    // calculates discounts based on which type is selected
+    if (discountType === '1plus1free') {
+        discountPercentage = calculateOnePlusOneDiscountPercentage(quantity, container);
+    } else if (discountType === 'percentage') {
+        discountPercentage = Number(container.querySelector('.discountInput').value);
+    }   
+
+    // DATA VALIDATION
+    let inputIsValid = true;
+
+    // price validation
+    if (!validatePrice(price, container)) {
+        inputIsValid = false;
+    }
+
+    // discount percentage validation
+    if (!validateDiscountPercentage(discountPercentage, discountType, container)) {
+        inputIsValid = false;
+    }
+
+    // quantity validation
+    if (!validateQuantity(quantity, container)) {
+        inputIsValid = false;
+    }
+
+    // shipping cost validation
+    if (!validateShippingCost(shippingCost, container)) {
+        inputIsValid = false;
+    }
+
+    // if any inputs are invalid, fuction returns
+    if (!inputIsValid) {
+        return;
+    }
+
+    // DATA IS PROCESSED
+    // all prices are in cents, unless variable name ends in "output"
+
+    // calculation variables
+    const oldPriceTotal = price * quantity;
+    const oldPriceTotalPlusShipping = oldPriceTotal + shippingCost;
+    const moneySaved = oldPriceTotal * discountPercentage / 100;
+    const discountedPriceTotal = oldPriceTotalPlusShipping - moneySaved;
+    const discountedPricePerItem = discountedPriceTotal / quantity;
+
+    //output variables
+    const oldPriceTotalOutput = (oldPriceTotal / 100).toFixed(2);
+    const moneySavedOutput = (moneySaved / 100).toFixed(2);
+    const discountedPriceTotalOutput = (discountedPriceTotal / 100).toFixed(2);
+    const discountedPricePerItemOutput = (discountedPricePerItem / 100).toFixed(2);
+    const priceOutput = (price / 100).toFixed(2);
+
+    // print output
+    container.querySelector('.calculationMessage').innerHTML= `
+        Totale prijs: <s>€${oldPriceTotalOutput}</s> €${discountedPriceTotalOutput} <br> 
+        Prijs per stuk: <s>€${priceOutput}</s> €${discountedPricePerItemOutput} <br><br>
+        Je bespaart: €${moneySavedOutput}`;
+}
+
+// FUNCTIONS THAT CHANGE THE LAYOUT OF THE CALCULATOR
+
+function changeDiscountInput(selector) {
+    // makes sure that the function uses the input from the calculator the button is in
+    // this is only relevant if there is more than 1 calculator on screen
+    const container = selector.closest('.calculatorContainer');
+
+    // which discount type is selected
+    const discountType = container.querySelector('.discountType').value;
+
+    // changes input layout for discount type "percentage"
+    if (discountType === 'percentage') {
+        container.querySelector('.discountContainer').innerHTML = `
+        <input type="text" class="discountInput" name="discountPercentage" placeholder="bijv. 50"></input>
+        `;
+    // changes layout for discount type "1plus1free"
+    } else if (discountType === '1plus1free') {
+        container.querySelector('.discountContainer').innerHTML = `
+        <div class="onePlusOne">
+            <input type="number" class="onePlusOnePayedItem" value="1"> 
+            <div>+</div> 
+            <input type="number" class="onePlusOneFreeItem" value="1">
+            <div>gratis<div>
+        <div>
+        `;
+    }  
+}
+
+// FUNCTIONS THAT CALCULATE ALTERNATE DISCOUNT PERCENTAGES
+
+function calculateOnePlusOneDiscountPercentage(quantity, container) {
+    // For 1+1, the first 1 are the full price items and the second 1 are the free items
+    const fullPriceItems = Number(container.querySelector('.onePlusOnePayedItem').value);
+    const freeItems = Number(container.querySelector('.onePlusOneFreeItem').value);
+
+    // optimalAmount is the smallest amount at which you have the maximum discount percentage
+    const optimalAmount = fullPriceItems + freeItems;
+    // restAmount is the amount of items that are bought at less than the maximum discount
+    const restAmount = quantity % optimalAmount;
+
+    // calculates how many items you actually get for free, even if you don't buy (a multiple of) the optimal amount
+    // example: if the discount is 1+1 and you buy 2, you get 1 free item, the discount is 50%
+    // example: if the discount is 1+1 and you buy 3, you get 1 free item, the discount is 33,3%
+    let actualFreeItems = 0;
+    if (quantity < optimalAmount) {
+        if (quantity > fullPriceItems) {
+            actualFreeItems = quantity - fullPriceItems;
+        }
+      } else {
+        actualFreeItems = ((quantity - restAmount) / (optimalAmount)) * freeItems;
+      }
+    return (actualFreeItems / quantity * 100);
+}
+
+// DATA VALIDATION FUNCTIONS
+
 function validatePrice(price, container) {
     // UserInput contains the actual string that the user typed in
     const userInput = container.querySelector('.priceInput').value;
@@ -103,153 +242,4 @@ function validateQuantity(quantity, container) {
         console.log('Quantity valid');
         return true;
     }
-}
-
-function calculateDiscount(button) {
-    // makes sure that the function uses the input from the calculator the button is in
-    // this is only relevant if there is more than one calculator on screen
-    const container = button.closest('.calculatorContainer');
-
-    // GET ALL NESCESSARY DATA FROM USER INPUT
-
-    // data from inputs that stay the same
-    const price = container.querySelector('.priceInput').value * 100;
-    const quantity = Number(container.querySelector('.quantityInput').value);
-    const discountType = container.querySelector('.discountType').value;
-    
-    // data from inputs that may differ depending on which menu-options are selected
-    let discountPercentage = 0;
-    let shippingCost = 0;
-    
-    // changes shipping cost only if an input is detected, otherwise it stays 0
-    if (container.querySelector('.shippingCostInput') !== null) {
-        shippingCost = container.querySelector('.shippingCostInput').value * 100;
-    }
-
-    // calculates discounts based on which type is selected
-    if (discountType === '1plus1free') {
-        discountPercentage = calculateOnePlusOneDiscountPercentage(quantity, container);
-    } else if (discountType === 'percentage') {
-        discountPercentage = Number(container.querySelector('.discountInput').value);
-    }   
-
-    // DATA VALIDATION
-    let inputIsValid = true;
-
-    // price validation
-    if (!validatePrice(price, container)) {
-        inputIsValid = false;
-    }
-
-    // discount percentage validation
-    if (!validateDiscountPercentage(discountPercentage, discountType, container)) {
-        inputIsValid = false;
-    }
-
-    // quantity validation
-    if (!validateQuantity(quantity, container)) {
-        inputIsValid = false;
-    }
-
-    // shipping cost validation
-    if (!validateShippingCost(shippingCost, container)) {
-        inputIsValid = false;
-    }
-
-    // if any inputs are invalid, fuction returns
-    if (!inputIsValid) {
-        return;
-    }
-
-    // DATA IS PROCESSED
-    // all prices are in cents, unless variable name ends in "output"
-
-    // calculation variables
-    const oldPriceTotal = price * quantity;
-    const oldPriceTotalPlusShipping = oldPriceTotal + shippingCost;
-    const moneySaved = oldPriceTotal * discountPercentage / 100;
-    const discountedPriceTotal = oldPriceTotalPlusShipping - moneySaved;
-    const discountedPricePerItem = discountedPriceTotal / quantity;
-
-    //output variables
-    const oldPriceTotalOutput = (oldPriceTotal / 100).toFixed(2);
-    const moneySavedOutput = (moneySaved / 100).toFixed(2);
-    const discountedPriceTotalOutput = (discountedPriceTotal / 100).toFixed(2);
-    const discountedPricePerItemOutput = (discountedPricePerItem / 100).toFixed(2);
-    const priceOutput = (price / 100).toFixed(2);
-
-    // print output
-    container.querySelector('.calculationMessage').innerHTML= `
-        Totale prijs: <s>€${oldPriceTotalOutput}</s> €${discountedPriceTotalOutput} <br> 
-        Prijs per stuk: <s>€${priceOutput}</s> €${discountedPricePerItemOutput} <br><br>
-        Je bespaart: €${moneySavedOutput}`;
-}
-
-function changeDiscountInput(selector) {
-    // makes sure that the function uses the input from the calculator the button is in
-    // this is only relevant if there is more than 1 calculator on screen
-    const container = selector.closest('.calculatorContainer');
-
-    // which discount type is selected
-    const discountType = container.querySelector('.discountType').value;
-
-    // changes input layout for discount type "percentage"
-    if (discountType === 'percentage') {
-        container.querySelector('.discountContainer').innerHTML = `
-        <input type="text" class="discountInput" name="discountPercentage" placeholder="bijv. 50"></input>
-        `;
-    // changes layout for discount type "1plus1free"
-    } else if (discountType === '1plus1free') {
-        container.querySelector('.discountContainer').innerHTML = `
-        <div class="onePlusOne">
-            <input type="number" class="onePlusOnePayedItem" value="1"> 
-            <div>+</div> 
-            <input type="number" class="onePlusOneFreeItem" value="1">
-            <div>gratis<div>
-        <div>
-        `;
-    }
-    
-}
-
-function calculateOnePlusOneDiscountPercentage(quantity, container) {
-    // For 1+1, the first 1 are the full price items and the second 1 are the free items
-    const fullPriceItems = Number(container.querySelector('.onePlusOnePayedItem').value);
-    const freeItems = Number(container.querySelector('.onePlusOneFreeItem').value);
-
-    // optimalAmount is the smallest amount at which you have the maximum discount percentage
-    const optimalAmount = fullPriceItems + freeItems;
-    // restAmount is the amount of items that are bought at less than the maximum discount
-    const restAmount = quantity % optimalAmount;
-
-    // calculates how many items you actually get for free, even if you don't buy (a multiple of) the optimal amount
-    // example: if the discount is 1+1 and you buy 2, you get 1 free item, the discount is 50%
-    // example: if the discount is 1+1 and you buy 3, you get 1 free item, the discount is 33,3%
-    let actualFreeItems = 0;
-    if (quantity < optimalAmount) {
-        if (quantity > fullPriceItems) {
-            actualFreeItems = quantity - fullPriceItems;
-        }
-      } else {
-        actualFreeItems = ((quantity - restAmount) / (optimalAmount)) * freeItems;
-      }
-    return (actualFreeItems / quantity * 100);
-}
-
-function addShippingCostInput(selector) {
-    // makes sure that the function uses the input from the calculator the button is in
-    // this is only relevant if there is more than 1 calculator on screen
-    const container = selector.closest('.calculatorContainer');
-
-    // checks what option is selected
-    const hasShippingCost = container.querySelector('.hasShippingCost').value;
-
-    // changes layout based on selection
-    if (hasShippingCost === 'yes') {
-        container.querySelector('.shippingInputContainer').innerHTML = '<input class="shippingCostInput" type="text">';
-    } else if (hasShippingCost === 'no') {
-        document.querySelector('.shippingInputContainer').innerHTML = '';
-        container.querySelector('.shippingCostError').innerHTML = '';
-    }
-
 }
